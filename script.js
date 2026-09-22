2739ed8faebc3fc4b67660bd7675ee0f7f01dcaf
Julio Salvador Girardi <julio.girardi@escola.pr.gov.br>
2026-09-22 08:50:52 -0300

    esta errado

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+// troca de tema
+        const botaoTema = document.querySelector("#botao-tema");
+        const temaSalvo = localStorage.getItem("tema");
+
+        if (temaSalvo === "escuro") {
+            document.body.classList.add("tema-escuro");
+            botaoTema.textContent = "☀ Tema";
+        }
+
+        botaoTema.addEventListener("click", function () {
+            document.body.classList.toggle("tema-escuro");
+
+            if (document.body.classList.contains("tema-escuro")) {
+                botaoTema.textContent = "☀ Tema";
+                localStorage.setItem("tema", "escuro");
+            } else {
+                botaoTema.textContent = "🌙 Tema";
+                localStorage.setItem("tema", "claro");
+            }
+        });
+
 
